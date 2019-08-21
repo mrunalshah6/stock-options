1f9f2673296ae231fa6c84b9ed2211515b5f7845
Mrunal Shah <mrunalshah6@gmail.com>
2019-08-21 01:01:43 -0400

    Updated scraper to parse options chain and the last price

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,15 +4,24 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'https://www.nasdaq.com/symbol/aapl/option-chain',
+        'https://www.nasdaq.com/symbol/aapl/option-chain?dateindex=1',
         
     ]
 
     def parse(self, response):
-        rows = response.xpath('//div [@class="OptionsChain-chart borderAll thin"]/table//tr') 
-        print(type(rows[5]))
+        rows = response.xpath('//div [@class="OptionsChain-chart borderAll thin"]/table/tr')
+        storage = {}
         for row in rows:
-         print(row.css('td').get()) 
+            #  print (row.get())
+            #  print(row.xpath('td/a/@href').get())
+            #  print(row.xpath('td/text()').get())
+             args = (row.xpath('td/a/@href').get(),row.xpath('td/text()').get())
+             print('Option chain url %r and last price %r' % args)
+             optionschain = row.xpath('td/a/@href').get()
+             lastprice = row.xpath('td/text()').get()
+             storage[optionschain[48:63]] = lastprice
+        return storage
+             
 
             
               
