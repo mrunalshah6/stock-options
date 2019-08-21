fe876b7793921a9fe1f6c25bac49114f85100956
Mrunal Shah <mrunalshah6@gmail.com>
2019-08-21 17:51:21 -0400

    added next page functionality

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,8 +4,9 @@ from ..items import TutorialItem
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    val = input("Enter your value: ") 
     start_urls = [
-        'https://www.nasdaq.com/symbol/aapl/option-chain?dateindex=1',
+        'https://www.nasdaq.com/symbol/{}/option-chain'.format(val),
         
     ]
 
@@ -14,9 +15,7 @@ class QuotesSpider(scrapy.Spider):
         rows = response.xpath('//div [@class="OptionsChain-chart borderAll thin"]/table/tr')
         
         for row in rows:
-            #  print (row.get())
-            #  print(row.xpath('td/a/@href').get())
-            #  print(row.xpath('td/text()').get())
+            
              args = (row.xpath('td/a/@href').get(),row.xpath('td/text()').get())
              print('Option chain url %r and last price %r' % args)
              optionschainscrap = row.xpath('td/a/@href').get()
@@ -25,8 +24,17 @@ class QuotesSpider(scrapy.Spider):
              items['optionschain'] = optionschain
              items ['lastprice'] = lastprice
              yield items
-             
+        
+        next_page = response.xpath ('//*[@id="quotes_content_left_lb_NextPage"]/@href').get()
+        if next_page is not None:
+            print("Onto next page")
+            yield response.follow (next_page,callback = self.parse)
+        
+        next_months = response.xpath('//*[@id="OptionsChain-dates"]/a')
+        for next_month in next_months:
+            print(next_month)
 
+        
             
               
         
